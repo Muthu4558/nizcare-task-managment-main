37f14277f0b67997e28ea4ec9012bdeb86a67238
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2024-11-27 18:35:06 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -94,7 +94,7 @@ const App = () => {
     <main className='w-full min-h-screen bg-[#f3f4f6]'>
       <Routes>
         <Route element={<Layout />}>
-          <Route path='/' element={<Navigate to='/dashboard' replace />} />
+          <Route path='*' element={<Navigate to='/dashboard' replace />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/tasks' element={<Tasks />} />
           <Route path='/completed/:status' element={<Tasks />} />
